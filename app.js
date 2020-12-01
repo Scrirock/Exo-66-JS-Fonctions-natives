350e1fdc3ed656d97eab01fd050d13e19b0adf41
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-01 19:38:20 +0100

    fdgh

--- app.js
+++ app.js
@@ -2,22 +2,22 @@ let entier = document.getElementById("entier")
 let flottant = document.getElementById("flottant");
 let nonNumerique = document.getElementById("non-numerique");
 let random = document.getElementById("random");
-let trunc = document.getElementById("trunc").innerHTML;
-let textReplace = document.getElementById("replace").innerHTML;
+let trunc = document.getElementById("trunc");
+let textReplace = document.getElementById("replace");
 let operande = 10;
 
 entier.innerHTML *= operande;
 
-flottant.innerHTML *= operande;
+flottant.innerHTML = parseFloat(flottant.innerHTML * operande + parseInt(entier.innerHTML));
 
 if (isNaN(nonNumerique)){
-    nonNumerique.innerHTML = "0";
+    nonNumerique.innerHTML = 0;
 }
 
 random.innerHTML = Math.trunc(Math.random() * 100);
 
-document.getElementById("trunc").innerHTML = Math.trunc(trunc);
+trunc.innerHTML = Math.trunc(trunc.innerHTML);
 
-document.getElementById("replace").innerHTML = textReplace.replace('Hello world !', "Pête Matthias");
+textReplace.innerHTML = textReplace.innerHTML.replace('world', "Pête Matthias");
 
 console.log(random());
