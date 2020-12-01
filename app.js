463a703177103b9299b1a1d2d240e2515488f334
Scrirock <73163680+Scrirock@users.noreply.github.com>
2020-12-01 17:20:23 +0100

    a.l.e.d

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+let entier = document.getElementById("entier")
+let flottant = document.getElementById("flottant");
+let nonNumerique = document.getElementById("non-numerique");
+let random = document.getElementById("random");
+let trunc = document.getElementById("trunc").innerHTML;
+let textReplace = document.getElementById("replace").innerHTML;
+let operande = 10;
+
+entier.innerHTML *= operande;
+
+flottant.innerHTML *= operande;
+
+if (isNaN(nonNumerique)){
+    nonNumerique.innerHTML = "0";
+}
+
+random.innerHTML = Math.trunc(Math.random() * 100);
+
+document.getElementById("trunc").innerHTML = Math.trunc(trunc);
+
+document.getElementById("replace").innerHTML = textReplace.replace('Hello world !', "Pête Matthias");
+
 console.log(random());
